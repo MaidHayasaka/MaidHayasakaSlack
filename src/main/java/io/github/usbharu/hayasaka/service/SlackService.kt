6fe3ee9053efa1e40bae47df53061b2a77bb4663
usbharu <64310155+usbharu@users.noreply.github.com>
2022-06-16 11:16:00 +0900

    add: SlackのイベントをMaidHayasakaのイベントにラップするように

--- src/main/java/io/github/usbharu/hayasaka/service/SlackService.kt
+++ src/main/java/io/github/usbharu/hayasaka/service/SlackService.kt
@@ -1,20 +1,82 @@
 package io.github.usbharu.hayasaka.service
 
+import com.slack.api.bolt.App
+import com.slack.api.bolt.socket_mode.SocketModeApp
+import com.slack.api.model.event.MessageEvent
+import com.slack.api.model.event.ReactionAddedEvent
 import io.github.usbharu.hayasaka.api.MessageOperation
 import io.github.usbharu.hayasaka.api.ReactionOperation
+import io.github.usbharu.hayasaka.core.EventManager
 import io.github.usbharu.hayasaka.core.service.Service
+import io.github.usbharu.hayasaka.event.Event
+import io.github.usbharu.hayasaka.event.EventType
+import io.github.usbharu.hayasaka.model.ChannelType
+import io.github.usbharu.hayasaka.model.MessageType
+import io.github.usbharu.hayasaka.slack.api.SlackMessageOperation
+import io.github.usbharu.hayasaka.slack.api.SlackReactionOperation
+import io.github.usbharu.hayasaka.slack.model.SlackChannel
+import io.github.usbharu.hayasaka.slack.model.SlackMessage
+import io.github.usbharu.hayasaka.slack.model.SlackReaction
+import io.github.usbharu.hayasaka.slack.model.SlackUser
+import io.github.usbharu.hayasaka.util.SlackUtil
 
 class SlackService : Service() {
+    private lateinit var app: App;
+    private lateinit var socketModeApp: SocketModeApp;
     override val messageOperation: MessageOperation
         get() {
-            TODO()
+            return SlackMessageOperation()
         }
     override val reactionOperation: ReactionOperation
         get() {
-            TODO()
+            return SlackReactionOperation()
         }
 
-    override fun init() {}
-    override fun start() {}
-    override fun stop() {}
+    override fun init() {
+        app = App()
+        app.event(MessageEvent::class.java) { req, context ->
+            EventManager.event(
+                Event(
+                    EventType.MESSAGE_ADD,
+                    SlackMessage(
+                        req.event.text,
+                        SlackUser(req.event.user),
+                        MessageType.PUBLIC,
+                        SlackChannel(req.event.channel, ChannelType.PUBLIC)
+                    )
+                )
+            )
+            context.ack()
+        }
+        app.event(ReactionAddedEvent::class.java) { req, context ->
+            EventManager.event(
+                Event(
+                    EventType.REACTION_ADD,
+                    SlackReaction(
+                        req.event.reaction,
+                        SlackUser(req.event.user),
+                        SlackMessage(
+                            SlackUtil.getMessageWithReplies(
+                                req.event.item.channel,
+                                req.event.eventTs
+                            ),
+                            SlackUser(req.event.user),
+                            MessageType.PUBLIC,
+                            SlackChannel(req.event.item.channel, ChannelType.PUBLIC)
+                        )
+                    )
+                )
+            )
+            context.ack()
+        }
+    }
+
+    override fun start() {
+        socketModeApp = SocketModeApp(app)
+        socketModeApp.start()
+    }
+
+    override fun stop() {
+        socketModeApp.stop()
+    }
 }
---
+++ src/main/java/io/github/usbharu/hayasaka/util/SlackUtil.kt
@@ -0,0 +1,58 @@
+package io.github.usbharu.hayasaka.util
+
+import com.slack.api.Slack
+import com.slack.api.methods.request.auth.AuthTestRequest
+import com.slack.api.methods.request.conversations.ConversationsHistoryRequest
+import com.slack.api.methods.request.conversations.ConversationsMembersRequest
+import com.slack.api.methods.request.conversations.ConversationsRepliesRequest
+import com.slack.api.methods.response.conversations.ConversationsMembersResponse
+import com.slack.api.model.Message
+
+object SlackUtil {
+    val slack: Slack = Slack.getInstance()
+    val token: String = System.getenv("SLACK_BOT_TOKEN")
+    fun isChannelMembers(channelId: String): Boolean {
+        val conversationsMembers: ConversationsMembersResponse =
+            slack.methods(token).conversationsMembers(
+                ConversationsMembersRequest.builder().channel(channelId).build()
+            )
+        return conversationsMembers.members.contains(getBotUserId())
+    }
+
+    fun getBotUserId(): String {
+        return slack.methods(token).authTest(AuthTestRequest.builder().build()).getUserId()
+    }
+
+    fun getMessage(channelId: String, timeStamp: String): String {
+        return slack.methods(token).conversationsHistory(
+            ConversationsHistoryRequest.builder().channel(channelId).latest(timeStamp)
+                .inclusive(true).limit(1).build()
+        ).messages[0].text
+    }
+
+    fun getMessageWithReplies(channelId: String, timeStamp: String): String {
+        val conversationsHistory =
+            slack.methods(token).conversationsHistory(ConversationsHistoryRequest.builder().build())
+        val messages = ArrayList<Message>()
+        for (message in conversationsHistory.messages) {
+            if (message.threadTs != null) {
+                messages.add(message)
+            } else if (message.ts.equals(timeStamp)) {
+                return message.text
+            }
+        }
+        for (message in messages) {
+            val replies = slack.methods(token).conversationsReplies(
+                ConversationsRepliesRequest.builder().ts(message.threadTs).channel(channelId)
+                    .build()
+            ).messages
+            for (reply in replies) {
+                if (reply.ts.equals(timeStamp)) {
+                    return reply.text
+                }
+            }
+
+        }
+        throw java.lang.IllegalArgumentException("Message not found channel : $channelId timeStamp : $timeStamp")
+    }
+}
